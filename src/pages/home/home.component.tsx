91debcbfb3d63cb7da9e73f414c6de503af3dbac
Alexandre_oliveiradev <alexandredev.frontend@gmail.com>
2024-05-02 22:34:30 -0300

    wip

--- src/pages/home/home.component.tsx
+++ src/pages/home/home.component.tsx
@@ -5,6 +5,20 @@ export default function Home() {
     <>
       <div className="container-home">
         <Header></Header>
+        <section className="section-home">
+          <div className="box">
+            <div style={{width: "min-content"}}>
+              <h1>Olá, sou o</h1>
+              <h1>Alexandre</h1>
+              <br />
+              <p style={{color: "#fff", fontSize: 20}}>
+                Desenvolvedor web full-stack pleno, amante de tecnologia, com
+                experiência no ramo de Bilhetagem eletrônica e E-commerces.
+              </p>
+            </div>
+          </div>
+          <div className="box"></div>
+        </section>
       </div>
     </>
   );
