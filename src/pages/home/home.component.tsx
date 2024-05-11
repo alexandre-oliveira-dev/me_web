249b632070c9e99f0503908babfcaaf418675bf1
Alexandre_oliveira <alexandredev.frontend@gmail.com>
2024-05-11 14:13:54 -0300

    lasted

--- src/pages/home/home.component.tsx
+++ src/pages/home/home.component.tsx
@@ -1,9 +1,18 @@
 import Header from "../../components/header/header";
 import "./styles.css";
 import {FaLinkedin, FaGithub, FaFileDownload} from "react-icons/fa";
-import CircleText from "../../components/circleText";
+import {CircleText} from "circle-text-component/src";
 
 export default function Home() {
+  function hoverImgs(key: number) {
+    const imgstags = document.querySelectorAll(".imgskill");
+    imgstags[key]?.setAttribute(
+      "title",
+      imgstags[key]?.getAttribute("alt") || ""
+    );
+    imgstags[key]?.classList.add("skillsHover");
+  }
+
   return (
     <>
       <div className="container-home">
@@ -39,7 +48,10 @@ export default function Home() {
                   donwload.click();
                 }}
               >
-                <CircleText></CircleText>
+                <CircleText
+                  textColor="#fff"
+                  text="Dowload cv - Download cv"
+                ></CircleText>
                 <div className="dowload-cv"></div>
                 <div
                   style={{
@@ -77,11 +89,8 @@ export default function Home() {
           <div
             style={{
               width: "100%",
-              height: "auto",
-              padding: "40px 0 0 0",
               display: "flex",
               justifyContent: "center",
-              alignItems: "center",
             }}
           >
             <h1 style={{color: "#fff", fontSize: 50}}>Minhas Skills</h1>
@@ -89,73 +98,259 @@ export default function Home() {
           <div className="box-skills">
             <h2>Back-end</h2>
             <br />
+            <br />
             <div
               style={{
                 display: "flex",
                 width: "100%",
-                justifyContent: "space-around",
+                height: "auto",
+                justifyContent: "center",
                 flexWrap: "wrap",
+                gap: 40,
               }}
             >
               <img
+                onMouseMove={() => hoverImgs(0)}
+                onMouseLeave={() => {
+                  document
+                    .querySelectorAll(".imgskill")[0]
+                    ?.classList.remove("skillsHover");
+                }}
+                className="imgskill"
                 style={{width: "70px"}}
                 src="https://skillicons.dev/icons?i=nodejs"
-                alt="skills"
+                alt="nodejs"
               />
               <img
+                onMouseMove={() => hoverImgs(1)}
+                onMouseLeave={() => {
+                  document
+                    .querySelectorAll(".imgskill")[1]
+                    ?.classList.remove("skillsHover");
+                }}
+                className="imgskill"
                 style={{width: "70px"}}
                 src="https://skillicons.dev/icons?i=express"
-                alt="skills"
+                alt="express"
               />
               <img
+                onMouseMove={() => hoverImgs(2)}
+                onMouseLeave={() => {
+                  document
+                    .querySelectorAll(".imgskill")[2]
+                    ?.classList.remove("skillsHover");
+                }}
+                className="imgskill"
                 style={{width: "70px"}}
                 src="https://skillicons.dev/icons?i=prisma"
-                alt="skills"
+                alt="prisma"
               />
               <img
+                onMouseMove={() => hoverImgs(3)}
+                onMouseLeave={() => {
+                  document
+                    .querySelectorAll(".imgskill")[3]
+                    ?.classList.remove("skillsHover");
+                }}
+                className="imgskill"
                 style={{width: "70px"}}
                 src="https://skillicons.dev/icons?i=graphql"
-                alt="skills"
+                alt="graphql"
               />
               <img
+                onMouseMove={() => hoverImgs(4)}
+                onMouseLeave={() => {
+                  document
+                    .querySelectorAll(".imgskill")[4]
+                    ?.classList.remove("skillsHover");
+                }}
+                className="imgskill"
                 style={{width: "70px"}}
                 src="https://skillicons.dev/icons?i=nest"
-                alt="skills"
+                alt="nestjs"
               />
               <img
+                onMouseMove={() => hoverImgs(5)}
+                onMouseLeave={() => {
+                  document
+                    .querySelectorAll(".imgskill")[5]
+                    ?.classList.remove("skillsHover");
+                }}
+                className="imgskill"
                 style={{width: "70px"}}
                 src="https://skillicons.dev/icons?i=postgres"
-                alt="skills"
+                alt="postgreSql"
               />
               <img
+                onMouseMove={() => hoverImgs(6)}
+                onMouseLeave={() => {
+                  document
+                    .querySelectorAll(".imgskill")[6]
+                    ?.classList.remove("skillsHover");
+                }}
+                className="imgskill"
                 style={{width: "70px"}}
                 src="https://skillicons.dev/icons?i=mysql"
-                alt="skills"
+                alt="MySql"
               />
               <img
+                onMouseMove={() => hoverImgs(7)}
+                onMouseLeave={() => {
+                  document
+                    .querySelectorAll(".imgskill")[7]
+                    ?.classList.remove("skillsHover");
+                }}
+                className="imgskill"
                 style={{width: "70px"}}
                 src="https://skillicons.dev/icons?i=mongodb"
-                alt="skills"
+                alt="mongoDb"
               />
               <img
+                onMouseMove={() => hoverImgs(8)}
+                onMouseLeave={() => {
+                  document
+                    .querySelectorAll(".imgskill")[8]
+                    ?.classList.remove("skillsHover");
+                }}
+                className="imgskill"
                 style={{width: "70px"}}
                 src="https://skillicons.dev/icons?i=firebase"
-                alt="skills"
+                alt="firebase"
               />
               <img
+                onMouseMove={() => hoverImgs(9)}
+                onMouseLeave={() => {
+                  document
+                    .querySelectorAll(".imgskill")[9]
+                    ?.classList.remove("skillsHover");
+                }}
+                className="imgskill"
                 style={{width: "70px"}}
                 src="https://skillicons.dev/icons?i=docker"
-                alt="skills"
+                alt="docker"
               />
               <img
+                onMouseMove={() => hoverImgs(10)}
+                onMouseLeave={() => {
+                  document
+                    .querySelectorAll(".imgskill")[10]
+                    ?.classList.remove("skillsHover");
+                }}
+                className="imgskill"
                 style={{width: "70px"}}
                 src="https://skillicons.dev/icons?i=azure"
-                alt="skills"
+                alt="azure"
               />
               <img
+                onMouseMove={() => hoverImgs(11)}
+                onMouseLeave={() => {
+                  document
+                    .querySelectorAll(".imgskill")[11]
+                    ?.classList.remove("skillsHover");
+                }}
+                className="imgskill"
                 style={{width: "70px"}}
                 src="https://skillicons.dev/icons?i=aws"
-                alt="skills"
+                alt="Aws"
+              />
+              <img
+                onMouseMove={() => hoverImgs(12)}
+                onMouseLeave={() => {
+                  document
+                    .querySelectorAll(".imgskill")[12]
+                    ?.classList.remove("skillsHover");
+                }}
+                className="imgskill"
+                style={{width: "70px"}}
+                src="https://skillicons.dev/icons?i=typescript"
+                alt="typescript"
+              />
+            </div>
+          </div>
+          <div className="box-skills">
+            <h2>Front-end</h2>
+            <br />
+            <br />
+            <div
+              style={{
+                display: "flex",
+                width: "100%",
+                justifyContent: "center",
+                flexWrap: "wrap",
+                gap: 40,
+              }}
+            >
+              <img
+                onMouseMove={() => hoverImgs(13)}
+                onMouseLeave={() => {
+                  document
+                    .querySelectorAll(".imgskill")[13]
+                    ?.classList.remove("skillsHover");
+                }}
+                className="imgskill"
+                style={{width: "70px"}}
+                src="https://skillicons.dev/icons?i=typescript"
+                alt="typescript"
+              />
+              <img
+                onMouseMove={() => hoverImgs(14)}
+                onMouseLeave={() => {
+                  document
+                    .querySelectorAll(".imgskill")[14]
+                    ?.classList.remove("skillsHover");
+                }}
+                className="imgskill"
+                style={{width: "70px"}}
+                src="https://skillicons.dev/icons?i=js"
+                alt="js"
+              />
+              <img
+                onMouseMove={() => hoverImgs(15)}
+                onMouseLeave={() => {
+                  document
+                    .querySelectorAll(".imgskill")[15]
+                    ?.classList.remove("skillsHover");
+                }}
+                className="imgskill"
+                style={{width: "70px"}}
+                src="https://skillicons.dev/icons?i=html"
+                alt="html"
+              />
+              <img
+                onMouseMove={() => hoverImgs(16)}
+                onMouseLeave={() => {
+                  document
+                    .querySelectorAll(".imgskill")[16]
+                    ?.classList.remove("skillsHover");
+                }}
+                className="imgskill"
+                style={{width: "70px"}}
+                src="https://skillicons.dev/icons?i=css"
+                alt="css"
+              />
+              <img
+                onMouseMove={() => hoverImgs(17)}
+                onMouseLeave={() => {
+                  document
+                    .querySelectorAll(".imgskill")[17]
+                    ?.classList.remove("skillsHover");
+                }}
+                className="imgskill"
+                style={{width: "70px"}}
+                src="https://skillicons.dev/icons?i=react"
+                alt="reactjs / Native"
+              />
+              <img
+                onMouseMove={() => hoverImgs(18)}
+                onMouseLeave={() => {
+                  document
+                    .querySelectorAll(".imgskill")[18]
+                    ?.classList.remove("skillsHover");
+                }}
+                className="imgskill"
+                style={{width: "70px"}}
+                src="https://skillicons.dev/icons?i=figma"
+                alt="figma"
               />
             </div>
           </div>
